9c73d08fee3e4ef0b2b93a00f9ea1a54066388fe
ihabz <ihab.zeedia@stanford.edu>
2023-03-23 10:43:43 -0700

    initial template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stanford Home
+
+This is a new EM to replace R2P2 dashboard.
---
+++ StanfordHome.php
@@ -0,0 +1,15 @@
+<?php
+namespace Stanford\StanfordHome;
+
+class StanfordHome extends \ExternalModules\AbstractExternalModule {
+    public function __construct() {
+        parent::__construct();
+        // Other code to run when object is instantiated
+    }
+
+    public function redcap_every_page_top( int $project_id ) {
+
+    }
+
+
+}
